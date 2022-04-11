ef1ccad2fd5660abd5967eefa817d5e8ad098770
Subham Kumar <subhamk@juniper.net>
2022-04-11 17:36:46 +0530

    spring bot

---
+++ src/main/java/com/shubham/bot/BotApplication.java
@@ -0,0 +1,19 @@
+package com.shubham.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class BotApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(BotApplication.class);
+	}
+}
